795c1211d3c1891dbf88cabddfef4fcfa42bc29e
Krikal <rigo.gyorgyj@gmail.com>
2025-11-18 16:35:44 +0100

    text center in contact brief

--- src/components/ContactBrief.tsx
+++ src/components/ContactBrief.tsx
@@ -1,7 +1,7 @@
 
 const ContactBrief = () => {
   return (
-    <div className="h-22 justify-items-center content-center bg-white/30">
+    <div className="h-22 justify-items-center content-center bg-white/30 text-center">
         <p className="md:text-xl">Időpont foglalás csak <span className="about-highlight">telefonon</span> vagy <span className="about-highlight">személyesen</span></p>
     </div>
   )
